f3cd7b3b16019cfbd44962964bdc9f0f0fea0a37
leodanave <leonardo.nave@sptech.school>
2022-11-08 16:24:06 -0300

    UPDATE - Alterações básicas

--- Códigos/public/js/funcoes.js
+++ Códigos/public/js/funcoes.js
@@ -27,7 +27,7 @@ function limparSessao() {
 // carregamento (loading)
 function aguardar() {
     var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
+    divAguardar.style.display = "none";
 }
 
 function finalizarAguardar(texto) {
