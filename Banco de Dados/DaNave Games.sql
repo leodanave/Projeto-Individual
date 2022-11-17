33d99fbf9513be7e53d59b769414bdd36ad7f2a8
leodanave <leonardo.nave@sptech.school>
2022-11-16 23:17:10 -0300

    UPDATE - Script do mySQL atualizado

---
+++ Banco de Dados/DaNave Games.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE danave;
+USE danave;
+
+CREATE TABLE imagem (
+idImagem INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
+endereco VARCHAR(300)
+);
+
+INSERT INTO imagem VALUES 
+	(null, 'Ava', 'assets\\images\\Personagens\\ava.png'),
+	(null, 'Black Wolf', 'assets\\images\\Personagens\\blackwolf.png'),
+	(null, 'Rezan', 'assets\\images\\Personagens\\rezan.png'),
+	(null, 'Romanov', 'assets\\images\\Personagens\\romanov.png'),
+	(null, 'Slingshot', 'assets\\images\\Personagens\\slingshot.png');
+    
+SELECT * FROM imagem;
+
+CREATE TABLE usuario (
+idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
+email VARCHAR(45),
+senha VARCHAR(45),
+fkImagem INT,
+FOREIGN KEY (fkImagem) REFERENCES imagem (idImagem)
+);
+
+select * from usuario;
+
+CREATE TABLE analytic (
+idAnalytic INT AUTO_INCREMENT,
+resultado CHAR(5),
+CONSTRAINT chkResultado CHECK (resultado IN ('true', 'false')),
+fkUsuario INT,
+FOREIGN KEY (fkUsuario) REFERENCES usuario (idUsuario),
+PRIMARY KEY (idAnalytic, fkUsuario)
+);
+
+select * from analytic;
+
+select count(resultado) as Ganhou from analytic where resultado = 'false' and fkUsuario = 1;
+
+CREATE TABLE comentario (
+idComentario INT AUTO_INCREMENT,
+titulo VARCHAR(100),
+descricao VARCHAR(250),
+fkUsuario INT, 
+FOREIGN KEY (fkUsuario) REFERENCES usuario (idUsuario),
+PRIMARY KEY (idComentario, fkUsuario)
+);
+
+
+
+
